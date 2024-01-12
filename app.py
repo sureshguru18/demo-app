e08631e8522e8832505c2243dc8fe52942922dad
sureshguru18 <sureshguru.kumar04@gmail.com>
2024-01-12 16:26:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+def home():
+    return 'Flask github hello world'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8070, debug=False)
